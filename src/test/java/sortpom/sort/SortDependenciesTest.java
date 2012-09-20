9678dbace3465d7b76db80927a337d3994275253
bjorn.ekryd@gmail.com <bjorn.ekryd@gmail.com@ebe3c262-98b0-11de-a15e-1ba91f903e93>
2012-09-20 20:18:27 +0000

    Added test for default plugin groupId.
    git-svn-id: https://sortpom.googlecode.com/svn/trunk@223 ebe3c262-98b0-11de-a15e-1ba91f903e93

--- src/test/java/sortpom/sort/SortDependenciesTest.java
+++ src/test/java/sortpom/sort/SortDependenciesTest.java
@@ -14,4 +14,13 @@ public class SortDependenciesTest {
                 .testFiles("/SortDep_input_simpleWithScope.xml", "/SortDep_expected_simpleWithScope2.xml");
     }
 
+    @Test
+    public final void defaultGroupIdForPluginsShouldWork() throws Exception {
+        SortPomImplUtil.create()
+                .defaultOrderFileName("custom_1.xml")
+                .sortPlugins("groupId,artifactId")
+                .lineSeparator("\n")
+                .nrOfIndentSpace(4)
+                .testFiles("/PluginDefaultName_input.xml", "/PluginDefaultName_expect.xml");
+    }
 }

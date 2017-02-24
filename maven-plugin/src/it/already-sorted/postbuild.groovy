ee5639886e4bdbe7459df91fc9c88a4b1faa1d51
Benoit Guerin <bguerin@netceler.com>
2017-02-24 22:01:15 +0100

    Rewrite integration tests so they match the default one

--- maven-plugin/src/it/already-sorted/postbuild.groovy
+++ maven-plugin/src/it/already-sorted/postbuild.groovy
@@ -5,8 +5,7 @@ backup = new File(basedir, 'pom.xml.bak')
 
 assert log.exists()
 assert log.text.contains('Sorting file ' + sorted.absolutePath)
-assert !log.text.contains('Saved backup of ' + sorted.absolutePath + ' to ' + backup.absolutePath)
-assert log.text.contains('Saved sorted pom file to ' + sorted.absolutePath)
+assert log.text.contains('Pom file is already sorted, exiting')
 assert !backup.exists()
 assert expected.text.replaceAll('@pom.version@', projectversion).tokenize('\n').equals(sorted.text.tokenize('\n'))
 

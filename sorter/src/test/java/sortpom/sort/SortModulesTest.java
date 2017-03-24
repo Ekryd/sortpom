bc2babbc9fca40234ade7374e82be195f74c451e
Monica Goward <monica@goward.com>
2017-03-24 09:17:17 +0000

    Dummy commit to trigger travis

--- sorter/src/test/java/sortpom/sort/SortModulesTest.java
+++ sorter/src/test/java/sortpom/sort/SortModulesTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import sortpom.util.SortPomImplUtil;
 
 public class SortModulesTest {
-    
     @Test
     public final void sortingOfPomFileWithSubmodulesShouldWork() throws Exception {
         SortPomImplUtil.create()

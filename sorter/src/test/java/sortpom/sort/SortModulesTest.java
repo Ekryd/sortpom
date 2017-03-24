edb6f40302979068e2555a9cf5d0f1dd49088b43
Monica Goward <monica@goward.com>
2017-03-24 09:07:08 +0000

    New unit test for sorted modules

--- sorter/src/test/java/sortpom/sort/SortModulesTest.java
+++ sorter/src/test/java/sortpom/sort/SortModulesTest.java
@@ -15,4 +15,13 @@ public class SortModulesTest {
                 .lineSeparator("\n")
                 .testFiles("/SortModules_input.xml", "/SortModules_expected.xml");
     }
+    @Test
+    public final void sortingOfPomFileWithSubmodulesNotEnabled() throws Exception {
+        SortPomImplUtil.create()
+                .sortProperties()
+                .sortPlugins("true")
+                .sortDependencies("true")
+                .lineSeparator("\n")
+                .testFiles("/SortModules_input.xml", "/SortModules_expected_notsorted.xml");
+    }
 }

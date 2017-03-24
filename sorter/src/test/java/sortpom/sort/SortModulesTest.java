17bd9e21de51f028781763c46f554594e0fb719e
Monica Goward <monica@goward.com>
2017-03-24 10:16:43 +0000

    Adding test to include extra elements within modules - unlikely scenario.

--- sorter/src/test/java/sortpom/sort/SortModulesTest.java
+++ sorter/src/test/java/sortpom/sort/SortModulesTest.java
@@ -15,6 +15,17 @@ public class SortModulesTest {
                 .testFiles("/SortModules_input.xml", "/SortModules_expected.xml");
     }
     @Test
+    public final void sortingOfPomFileWithSubmodulesAndExtraElementsShouldWork() throws Exception {
+        SortPomImplUtil.create()
+                .defaultOrderFileName("sortOrderFiles/extra_dummy_tags.xml")
+                .sortProperties()
+                .sortPlugins("true")
+                .sortModules()
+                .sortDependencies("true")
+                .lineSeparator("\n")
+                .testFiles("/SortModules_input_extra_elements.xml", "/SortModules_expected_extra_elements.xml");
+    }
+    @Test
     public final void sortingOfPomFileWithSubmodulesNotEnabled() throws Exception {
         SortPomImplUtil.create()
                 .sortProperties()

53de50b139ebac1a1871638d6bc1a6b1106ac4f8
Björn Ekryd <bjornatekryd.se>
2020-01-07 23:56:34 +0100

    Coverage should still be allowed for pull requests

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 if [ "${SONAR_SCANNER_HOME}" != "" ]; then
     COMMAND="mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -B"
 else
-    COMMAND="mvn clean package -B"
+    COMMAND="mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package -B"
 fi
 
 echo ${COMMAND}

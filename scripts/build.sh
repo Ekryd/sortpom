e1c8d45d593ee5d076b7807c5fd5d414a92c7628
Björn Ekryd <bjornatekryd.se>
2020-01-10 20:32:23 +0100

    Run Sonar only for JDK11. Official SonarCloud Badge

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 echo Sonar scanner home: "${SONAR_SCANNER_HOME}"
 echo Java home: "${JAVA_HOME}"
 
-if [ "${SONAR_SCANNER_HOME}" != "" ]; then
+if [ "${SONAR_SCANNER_HOME}" != "" ] && [ "${JAVA_HOME}" = "/home/travis/oraclejdk11" ]; then
     COMMAND="mvn clean jacoco:prepare-agent package jacoco:report sonar:sonar -B"
 else
     COMMAND="mvn clean jacoco:prepare-agent package -B"

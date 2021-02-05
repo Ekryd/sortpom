ef1c4f72013ca97fcba9cfc808b9f077ea52547c
Björn Ekryd <bjornatekryd.se>
2021-02-05 22:42:35 +0100

    #85 Added test for edge case

--- sorter/src/test/java/sortpom/sort/IndentationTest.java
+++ sorter/src/test/java/sortpom/sort/IndentationTest.java
@@ -1,5 +1,7 @@
 package sortpom.sort;
 
+import org.jdom.Document;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import sortpom.XmlOutputGenerator;
@@ -13,7 +15,7 @@ class IndentationTest {
 
     @ParameterizedTest()
     @ValueSource(ints = {0, 1, 4, -1})
-    final void differentIndentsShouldWork(int indent) throws Exception {
+    void differentIndentsShouldWork(int indent) throws Exception {
         String expectedFile = "/SortModules_expectedIndent" + indent + ".xml";
         SortPomImplUtil.create()
                 .sortProperties()
@@ -40,7 +42,7 @@ class IndentationTest {
                 .testFiles("/SortModules_input.xml", expectedFile);
     }
 
-    
+
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 4, -1})
     void indentSchemaLocationShouldAddNewlineAndIndentation(int indent) {
@@ -55,14 +57,42 @@ class IndentationTest {
         String actual = xmlOutputGenerator.getSortedXml(createXmlProjectFragment());
         String indentChars = "";
         switch (indent) {
-            case 1 : indentChars = " "; break;
-            case 4 : indentChars = "    "; break;
-            case -1 : indentChars = "\t"; break;
+            case 1:
+                indentChars = " ";
+                break;
+            case 4:
+                indentChars = "    ";
+                break;
+            case -1:
+                indentChars = "\t";
+                break;
         }
         assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + lineSeparator +
                 "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"" + lineSeparator
                 + indentChars + indentChars + " xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd\">" + lineSeparator
                 + indentChars + "<Gurka xmlns=\"\"></Gurka>" + lineSeparator
+                + "</project>" + lineSeparator, actual);
+    }
+
+    @Test
+    void otherAttributeShouldNotBeIndented() {
+        XmlOutputGenerator xmlOutputGenerator = new XmlOutputGenerator();
+        String lineSeparator = "\n";
+        xmlOutputGenerator.setup(PluginParameters.builder()
+                .setEncoding("UTF-8")
+                .setFormatting(lineSeparator, true, true, false)
+                .setIndent(2, false, true)
+                .build());
+
+        Document xmlProjectFragment = createXmlProjectFragment();
+        xmlProjectFragment.getRootElement().getChild("Gurka").setAttribute("key", "value");
+        String actual = xmlOutputGenerator.getSortedXml(xmlProjectFragment);
+        String indentChars = "  ";
+
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + lineSeparator +
+                "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"" + lineSeparator
+                + indentChars + indentChars + " xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd\">" + lineSeparator
+                + indentChars + "<Gurka xmlns=\"\" key=\"value\"></Gurka>" + lineSeparator
                 + "</project>" + lineSeparator, actual);
     }
 }

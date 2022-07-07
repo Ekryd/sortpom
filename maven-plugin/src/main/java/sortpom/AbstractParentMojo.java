5f5bd64bd3be38fb4940978b2667b893481bf01c
Björn Ekryd <bjornatekryd.se>
2022-07-07 17:13:53 +0200

    Fixed the mojo description test

--- maven-plugin/src/main/java/sortpom/AbstractParentMojo.java
+++ maven-plugin/src/main/java/sortpom/AbstractParentMojo.java
@@ -98,7 +98,7 @@ abstract class AbstractParentMojo extends AbstractMojo {
    * Comma-separated ordered list how dependencies in dependency management should be sorted.
    * Example: scope,groupId,artifactId. If scope is specified in the list then the scope ranking is
    * COMPILE, PROVIDED, SYSTEM, RUNTIME, IMPORT and TEST. The list can be separated by ",;:". It
-   * would take precedence if present and would fallback to {@link #sortDependencies} if not
+   * would take precedence if present and would fall back to {@link #sortDependencies} if not
    * present.
    */
   @Parameter(property = "sort.sortDependencyManagement")

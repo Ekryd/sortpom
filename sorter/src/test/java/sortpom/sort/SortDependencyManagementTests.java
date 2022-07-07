974320b292de440631e6212e8ef6b2556a9c847b
Björn Ekryd <bjornatekryd.se>
2022-07-07 20:48:58 +0200

    Added a test where dependency management sorting is disabled

--- sorter/src/test/java/sortpom/sort/SortDependencyManagementTests.java
+++ sorter/src/test/java/sortpom/sort/SortDependencyManagementTests.java
@@ -38,4 +38,16 @@ public class SortDependencyManagementTests {
             "/SortDepManagement_input_withDependencies.xml",
             "/SortDepManagement_expected_withDependencies.xml");
   }
+
+  @Test
+  final void dependencyManagementShouldBeUnaffected() throws Exception {
+    SortPomImplUtil.create()
+        .customSortOrderFile("custom_1.xml")
+        .sortDependencies("scope,GROUPID,artifactId")
+        .sortDependencyManagement("none")
+        .lineSeparator("\n")
+        .testFiles(
+            "/SortDepManagement_input_withBomDeps.xml",
+            "/SortDepManagement_expected_withBomDeps.xml");
+  }
 }

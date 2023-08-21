daf85b8f815b87001bd1dbcca07d1bd44632243b
Björn Ekryd <Ekryd@users.noreply.github.com>
2023-08-21 16:41:08 +0200

    Issue 367 setup maven 4 build pipe (#369)
    * Adding GitHub workflow to build plugin with Maven 4
    * Make mojo-description test use file instead of build log.

--- maven-plugin/src/it/mojo-description/postbuild.groovy
+++ maven-plugin/src/it/mojo-description/postbuild.groovy
@@ -1,7 +1,7 @@
-log = new File(basedir, 'build.log')
-expected_log = new File(basedir, 'expected.log')
+description = new File(basedir, 'description.txt')
+expected_description = new File(basedir, 'expected_description.txt')
 
-assert log.exists()
-assert log.text.replaceAll('\r','').contains(expected_log.text.replaceAll('@pom.version@', projectversion).replaceAll('\r',''))
+assert description.exists()
+assert description.text.replaceAll('\r','').contains(expected_description.text.replaceAll('@pom.version@', projectversion).replaceAll('\r',''))
 
 return true

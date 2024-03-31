c561ece8fb813b0ba32f1c4ac2a7119d8975719f
sivasubramanian8055 <yosiva137@gmail.com>
2024-03-31 15:44:00 -0300

    test case fix

--- sorter/src/main/java/sortpom/SortPomImpl.java
+++ sorter/src/main/java/sortpom/SortPomImpl.java
@@ -37,13 +37,33 @@ public class SortPomImpl {
 
   // Extracted method to warn about deprecated arguments
   private void warnAboutDeprecatedArguments(PluginParameters pluginParameters) {
-    warnAboutDeprecatedValue("sortDependencies", pluginParameters.sortDependencies.isDeprecatedValueTrue(), pluginParameters.sortDependencies.isDeprecatedValueFalse(), "The 'true' value in sortDependencies is not supported anymore, please use value 'groupId,artifactId' instead.", "The 'false' value in sortDependencies is not supported anymore, please use empty value '' or omit sortDependencies instead.");
-    warnAboutDeprecatedValue("sortDependencyExclusions", pluginParameters.sortDependencies.isDeprecatedValueTrue(), pluginParameters.sortDependencies.isDeprecatedValueFalse(), "The 'true' value in sortDependencyExclusions is not supported, please use value 'groupId,artifactId' instead.", "The 'false' value in sortDependencyExclusions is not supported, please use empty value '' or omit sortDependencyExclusions instead.");
-    warnAboutDeprecatedValue("sortPlugins", pluginParameters.sortDependencies.isDeprecatedValueTrue(), pluginParameters.sortDependencies.isDeprecatedValueFalse(), "The 'true' value in sortPlugins is not supported anymore, please use value 'groupId,artifactId' instead.", "The 'false' value in sortPlugins is not supported anymore, please use empty value '' or omit sortPlugins instead.");
+    warnAboutDeprecatedValue(
+            "sortDependencies",
+            pluginParameters.sortDependencies.isDeprecatedValueTrue(),
+            pluginParameters.sortDependencies.isDeprecatedValueFalse(),
+            "The 'true' value in sortDependencies is not supported anymore, please use value 'groupId,artifactId' instead.",
+            "The 'false' value in sortDependencies is not supported anymore, please use empty value '' or omit sortDependencies instead.");
+    warnAboutDeprecatedValue(
+            "sortDependencyExclusions",
+            pluginParameters.sortDependencyExclusions.isDeprecatedValueTrue(),
+            pluginParameters.sortDependencyExclusions.isDeprecatedValueFalse(),
+            "The 'true' value in sortDependencyExclusions is not supported, please use value 'groupId,artifactId' instead.",
+            "The 'false' value in sortDependencyExclusions is not supported, please use empty value '' or omit sortDependencyExclusions instead.");
+    warnAboutDeprecatedValue(
+            "sortPlugins",
+            pluginParameters.sortPlugins.isDeprecatedValueTrue(),
+            pluginParameters.sortPlugins.isDeprecatedValueFalse(),
+            "The 'true' value in sortPlugins is not supported anymore, please use value 'groupId,artifactId' instead.",
+            "The 'false' value in sortPlugins is not supported anymore, please use empty value '' or omit sortPlugins instead.");
   }
 
   // Extracted method to warn about deprecated values for individual parameters
-  private void warnAboutDeprecatedValue(String parameterName, boolean isTrueDeprecated, boolean isFalseDeprecated, String trueMessage, String falseMessage) {
+  private void warnAboutDeprecatedValue(
+          String parameterName,
+          boolean isTrueDeprecated,
+          boolean isFalseDeprecated,
+          String trueMessage,
+          String falseMessage) {
     if (isTrueDeprecated) {
       throw new FailureException(trueMessage);
     }
@@ -70,6 +90,7 @@ public class SortPomImpl {
   /** Verify that the pom-file is sorted regardless of formatting */
   public void verifyPom() {
     var xmlOrderedResult = getVerificationResult();
+    // Rename method/variable
     performVerificationResult(xmlOrderedResult);
   }
 
@@ -87,32 +108,50 @@ public class SortPomImpl {
     }
     return xmlOrderedResult;
   }
+
   // Rename method/variable
   private void performVerificationResult(XmlOrderedResult xmlOrderedResult) {
     if (!xmlOrderedResult.isOrdered()) {
-      switch (verifyFailType) {
-        case WARN:
-          log.warn(xmlOrderedResult.getErrorMessage());
-          sortPomService.saveViolationFile(xmlOrderedResult);
-          log.warn(String.format(TEXT_FILE_NOT_SORTED, pomFile.getAbsolutePath()));
-          break;
-        case SORT:
-          log.info(xmlOrderedResult.getErrorMessage());
-          sortPomService.saveViolationFile(xmlOrderedResult);
-          log.info(String.format(TEXT_FILE_NOT_SORTED, pomFile.getAbsolutePath()));
-          log.info("Sorting file " + pomFile.getAbsolutePath());
-          sortPomService.generateSortedXml();
-          sortPomService.createBackupFile();
-          sortPomService.saveGeneratedXml();
-          log.info("Saved sorted pom file to " + pomFile.getAbsolutePath());
-          break;
-        case STOP:
-          log.error(xmlOrderedResult.getErrorMessage());
-          sortPomService.saveViolationFile(xmlOrderedResult);
-          log.error(String.format(TEXT_FILE_NOT_SORTED, pomFile.getAbsolutePath()));
-          throw new FailureException(
-                  String.format(TEXT_FILE_NOT_SORTED, pomFile.getAbsolutePath()));
-      }
+      // Decompose conditional
+      handleUnorderedXml(xmlOrderedResult);
     }
+  }
+
+  private void handleUnorderedXml(XmlOrderedResult xmlOrderedResult) {
+    switch (verifyFailType) {
+      case WARN:
+        handleWarning(xmlOrderedResult);
+        break;
+      case SORT:
+        handleSort(xmlOrderedResult);
+        break;
+      case STOP:
+        handleStop(xmlOrderedResult);
+        break;
+    }
+  }
+
+  private void handleWarning(XmlOrderedResult xmlOrderedResult) {
+    log.warn(xmlOrderedResult.getErrorMessage());
+    sortPomService.saveViolationFile(xmlOrderedResult);
+    log.warn(String.format(TEXT_FILE_NOT_SORTED, pomFile.getAbsolutePath()));
+  }
+
+  private void handleSort(XmlOrderedResult xmlOrderedResult) {
+    log.info(xmlOrderedResult.getErrorMessage());
+    sortPomService.saveViolationFile(xmlOrderedResult);
+    log.info(String.format(TEXT_FILE_NOT_SORTED, pomFile.getAbsolutePath()));
+    log.info("Sorting file " + pomFile.getAbsolutePath());
+    sortPomService.generateSortedXml();
+    sortPomService.createBackupFile();
+    sortPomService.saveGeneratedXml();
+    log.info("Saved sorted pom file to " + pomFile.getAbsolutePath());
+  }
+
+  private void handleStop(XmlOrderedResult xmlOrderedResult) {
+    log.error(xmlOrderedResult.getErrorMessage());
+    sortPomService.saveViolationFile(xmlOrderedResult);
+    log.error(String.format(TEXT_FILE_NOT_SORTED, pomFile.getAbsolutePath()));
+    throw new FailureException(String.format(TEXT_FILE_NOT_SORTED, pomFile.getAbsolutePath()));
   }
 }
